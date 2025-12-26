f5cdf8caaaf546ecc6c558fec4fb2a6198d19f26
Thanseer-steyp <thanseersteyp@gmail.com>
2025-12-26 21:57:59 +0530

    Project url modified

--- backend/ecom/ecom/urls.py
+++ backend/ecom/ecom/urls.py
@@ -4,9 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/register/', include('api.v1.register.urls')),
-    path('api/v1/user/', include('api.v1.user.urls')),
-    path('api/v1/manager/', include('api.v1.manager.urls')),
-    path('api/v1/public/', include('api.v1.public.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("api/v1/", include([
+        path("register/", include("api.v1.register.urls")),
+        path("user/", include("api.v1.user.urls")),
+        path("manager/", include("api.v1.manager.urls")),
+        path("public/", include("api.v1.public.urls")),
+    ])),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
